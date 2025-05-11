417ae75c3e1bf7f9d7b8049aad490f97a026cb00
felipebrgs <felipeborgaco@hotmail.com>
2025-05-10 22:09:12 -0300

    .

--- app/web/src/app/(home)/page.tsx
+++ app/web/src/app/(home)/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='w-8/12 mx-auto mt-10 h-lvh'>
+            <div className='flex w-full h-full bg-slate-100 text-slate-800 rounded'>
+                <h1>Teste</h1>
+            </div>
         </div>
     );
 }
--- app/web/src/app/layout.tsx
+++ app/web/src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
             suppressHydrationWarning
         >
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${geistSans.variable} ${geistMono.variable} antialiased `}
             >
                 <ThemeProvider
                     attribute='class'
